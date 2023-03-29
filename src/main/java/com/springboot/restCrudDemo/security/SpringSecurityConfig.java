3349c65a3cb4113b76a5c602007fbe7590277c88
Ashwini S Kumar <ashwini.s@healsoftware.ai>
2023-03-29 09:42:23 +0530

    Added Spring Security.

--- src/main/java/com/springboot/restCrudDemo/security/SpringSecurityConfig.java
+++ src/main/java/com/springboot/restCrudDemo/security/SpringSecurityConfig.java
@@ -7,11 +7,54 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+        http.httpBasic();
+        //cross site request forgery
+        http.csrf().disable();
 
+        return http.build();
+    }
+
+    // Using custom tables members and roles
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id,pw,active from members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    // Added users and authorities in DB, no more hardcoding
+    /*
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    */
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -32,22 +75,6 @@ public class SpringSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john,mary,susan);
-    }
+    }*/
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
-        );
-        http.httpBasic();
-        //cross site request forgery
-        http.csrf().disable();
-
-        return http.build();
-    }
 }
